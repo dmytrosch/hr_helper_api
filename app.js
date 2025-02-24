52eca45556e87aaf173cdb7b8c5d056da2921a73
Dmytro Shcherbonos <dimaus.sch@gmail.com>
2025-02-24 18:34:40 +0200

    format

--- app.js
+++ app.js
@@ -1,34 +1,30 @@
-import dotenv from 'dotenv'
-import express from 'express'
-import db from './db.js'
-import { InvalidParamError, NotFoundError } from './errors.js'
-import employeesRouter from './routes/employees.js'
-import projectsRouter from './routes/projects.js'
-import positionsRouter from './routes/positions.js'
+import dotenv from "dotenv";
+import express from "express";
+import db from "./db.js";
+import { InvalidParamError, NotFoundError } from "./errors.js";
+import employeesRouter from "./routes/employees.js";
+import projectsRouter from "./routes/projects.js";
+import positionsRouter from "./routes/positions.js";
 
-dotenv.config()
+dotenv.config();
 
-const app = express()
+const app = express();
 app.use(express.json());
 
-db.initDB()
+db.initDB();
 
-app.use('/employees', employeesRouter)
-app.use('/projects', projectsRouter)
-app.use('/positions', positionsRouter)
-
-
-
+app.use("/employees", employeesRouter);
+app.use("/projects", projectsRouter);
+app.use("/positions", positionsRouter);
 
 app.use((error, req, res, next) => {
-  let status = 500
-  if (
-    error instanceof InvalidParamError ||
-    error instanceof NotFoundError
-  ) {
+  let status = 500;
+  if (error instanceof InvalidParamError || error instanceof NotFoundError) {
     status = error.status;
   }
   res.status(status).send({ message: error.message });
-})
+});
 
-app.listen(process.env.PORT, () => console.log(`API started at port ${process.env.PORT}`))
+app.listen(process.env.PORT, () =>
+  console.log(`API started at port ${process.env.PORT}`)
+);
