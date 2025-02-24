46e483c9add8f312178845b1cc281ec29b9b246d
Dmytro Shcherbonos <dimaus.sch@gmail.com>
2025-02-24 22:53:36 +0200

    added prisma

--- apollo.js
+++ apollo.js
@@ -1,14 +1,19 @@
 import { ApolloServer } from "apollo-server-express";
+import { PrismaClient } from "@prisma/client";
 import fs from "fs";
 import path from "path";
 import resolvers from "./resolvers/index.js";
 
+const prisma = new PrismaClient();
 const typeDefs = fs.readFileSync(path.resolve("./schema.graphql"), "utf-8");
 
 async function startApolloServer(app) {
   const server = new ApolloServer({
     typeDefs,
     resolvers,
+    context: {
+      prisma,
+    },
   });
 
   await server.start();
