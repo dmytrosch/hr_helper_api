943c2d87e15d2e422786cfee1016be4101d0345b
Dmytro Shcherbonos <dimaus.sch@gmail.com>
2025-03-05 15:08:17 +0200

    employee mutations: create, update, delete

--- resolvers/mutation.js
+++ resolvers/mutation.js
@@ -1,7 +1,9 @@
+import employeeMutations from "./mutations/employeeMutations.js"
 import positionMutations from "./mutations/positionMutations.js"
 
 const Mutation = {
 ...positionMutations,
+...employeeMutations
 }
 
 export default Mutation
